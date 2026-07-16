7c8c0fd60c773990732f156ce8c1c050cea2903b
Soulxcode <249457864+Soulxcode@users.noreply.github.com>
2026-07-16 14:06:59 +0100

    Mudar logica das peças para mais facil uso

--- include/piece.h
+++ include/piece.h
@@ -1,24 +1,8 @@
 #pragma once
 
 //Todos os tipos das pecas
-enum Piece
+class Piece
 {
-    EMPTY,
-
-    //Brancas
-    WPAWN,
-    WKNIGHT,
-    WBISHOP,
-    WROOK,
-    WQUEEN,
-    WKING,
-
-    //Pretas
-    BPAWN,
-    BKNIGHT,
-    BBISHOP,
-    BROOK,
-    BQUEEN,
-    BKING
+    
 };
 
