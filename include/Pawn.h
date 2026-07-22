c0adb4829dafc58f427ffcd1d6e76eec3e0a7d3e
Soulxcode <249457864+Soulxcode@users.noreply.github.com>
2026-07-22 10:50:08 +0100

    Class abstrata de Piece, começo de Pawn

--- include/Pawn.h
+++ include/Pawn.h
@@ -1,1 +1,11 @@
 #pragma once
+#include "Piece.h"
+
+class Pawn : public Piece {
+    public:
+        //Construtor
+        Pawn(Color pieceColor) : Piece(pieceColor){}
+
+        
+
+};
--- include/piece.h
+++ include/piece.h
@@ -1,8 +1,25 @@
 #pragma once
+#include "Color.h"
 
 //Todos os tipos das pecas
 class Piece
 {
+    public:
+        //Destructor é usado automaticamente
+        virtual ~Piece() = default;
+
+        //Verifica se o movimento é válido
+        virtual bool isValidMove(Position fromPosition, Position toPosition) const = 0;
+
+        //Devolve a cor da peça
+        virtual Color getColor() const = 0;
     
+    protected:
+        //Construtor  
+        Piece(Color pieceColor) : color(pieceColor){}
+
+    private:
+        //Cor da peça
+        Color color;
 };
 
