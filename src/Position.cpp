a9cb1cb911b67ec428baf464a5100bcd5de738b2
Soulxcode <249457864+Soulxcode@users.noreply.github.com>
2026-07-22 15:20:27 +0100

    Criacao de cpp das Pecas e acabar logica de Position

---
+++ src/Position.cpp
@@ -0,0 +1,14 @@
+#include <Position.h>
+
+//Construtor
+Position::Position(int row, int col) : row(row), col(col) {}
+
+//Retorna a coluna
+int Position::getCol() const {
+    return col;
+}
+
+//Retorna a linha
+int Position::getRow() const {
+    return row;
+}
