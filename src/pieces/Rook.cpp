72aaa09f74ab9e53df0bf7685255eb72a985c771
Soulxcode <249457864+Soulxcode@users.noreply.github.com>
2026-07-30 16:37:08 +0100

    Começar a validar movimentos das peças

--- src/pieces/Rook.cpp
+++ src/pieces/Rook.cpp
@@ -2,6 +2,15 @@
 
 bool Rook::isValidMove(const Move &move, const Board &board) const
 {
-    // TODO: actual king move logic
-    return false;
+    int startRow = move.start.getRow();
+    int startCol = move.start.getCol();
+
+    int destRow = move.destination.getRow();
+    int destCol = move.destination.getCol();
+
+    if(destRow != startRow || destCol != startCol){
+        return false;
+    }
+    
+
 }
